bde8050c89e49f63787f381a216c866f45c3f211
Alexander.R <ar-mob@ya.ru>
2025-09-24 15:40:21 +0300

    commit

--- c_kill_manual.sh
+++ c_kill_manual.sh
@@ -3,6 +3,7 @@
 # Переменная $* содержит все параметры, введённые в командной строке, в виде единого «слова».
 # В переменной $@ параметры разбиты на отдельные «слова». Эти параметры можно перебирать в циклах.
 
+procs=$*;
 premsg='kill manual';
 
 PGPASSWORD=${PASSWORD} psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d pglogger -At -c " \
@@ -17,7 +18,7 @@ PGPASSWORD=${PASSWORD} psql -h ${HOST} -p ${PORT} -U ${USERNAME} -d pglogger -At
                 ,datid ,datname ,pid ,usesysid ,usename ,application_name ,client_addr ,client_hostname ,client_port \
                 ,backend_start ,xact_start ,query_start ,state_change ,wait_event_type ,wait_event ,state ,backend_xid ,backend_xmin ,backend_type ,query \
         FROM pg_catalog.pg_stat_activity \
-        WHERE pid in ( " $* " ) \
+        WHERE pid in ( ${procs} ) \
 			AND backend_type = 'client backend' \
 			AND pid <> pg_backend_pid() \
         ;" &>&1 | ts "[${premsg}] " ;
